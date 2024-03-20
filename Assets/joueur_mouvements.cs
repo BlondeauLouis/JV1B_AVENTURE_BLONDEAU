6390076c85c4b735e8ae14a1f9042eb10645d478
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2024-03-20 16:22:38 +0100

    controles perso

--- Assets/joueur_mouvements.cs
+++ Assets/joueur_mouvements.cs
@@ -1,34 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class controles : MonoBehaviour
+public class Controles : MonoBehaviour
 {
     public float moveSpeed = 5f; // Vitesse de déplacement du joueur
 
-    public string horizontalAxis = "Horizontal"; // Axe de déplacement horizontal
-    public string verticalAxis = "Vertical"; // Axe de déplacement vertical
-
     private Rigidbody2D rb;
-    // Start is called before the first frame update
+
     void Start()
     {
         DontDestroyOnLoad(this);
-
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis(horizontalAxis);
-        float moveVertical = Input.GetAxis(verticalAxis);
-
-        // Si aucun mouvement n'est détecté via la manette, utilisez les touches du clavier
-        if (moveHorizontal == 0f)
-            moveHorizontal = Input.GetAxis("HorizontalKeyboard");
-        if (moveVertical == 0f)
-            moveVertical = Input.GetAxis("VerticalKeyboard");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
         // Calculer le vecteur de mouvement
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
