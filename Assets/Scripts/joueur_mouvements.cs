af5fc0808dc6039cc4b3d9b999d8affb34a55aa9
BlondeauLouis <loup.blondeau@gmail.com>
2024-04-17 15:06:25 +0200

    UI inventaire

--- Assets/Scripts/joueur_mouvements.cs
+++ Assets/Scripts/joueur_mouvements.cs
@@ -127,7 +127,6 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyDown(inventoryKey))
         {
-            // Inversez l'état de l'inventaire
             inventoryUI.SetActive(!inventoryUI.activeSelf);
         }
     }
